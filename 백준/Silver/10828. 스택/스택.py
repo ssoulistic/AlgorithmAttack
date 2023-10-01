30ffdd74b00e4f7676a1515d2454c9261d26c10f
ssoulistic <93485617+ssoulistic@users.noreply.github.com>
2023-10-01 12:47:49 +0900

    [Silver IV] Title: 스택, Time: 72 ms, Memory: 34176 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,45 @@
+from collections import deque
+import sys
+n=int(sys.stdin.readline())
+
+class quing():
+    def __init__(self):
+        self.queue=deque()
+    
+    def push(self,number):
+        self.queue.append(number)
+
+    def pop(self):
+        if self.queue:
+            print(self.queue.pop())
+        else:
+            print(-1)
+
+    def size(self):
+        print(len(self.queue))
+    
+    def empty(self):
+        if self.queue:
+            print(0)
+        else:
+            print(1)
+    
+    def top(self):
+        if self.queue:
+            print(self.queue[-1])
+        else:
+            print(-1)
+    
+Q=quing()
+for _ in range(n):
+    command=sys.stdin.readline().split()
+    if command[0]=="push":
+        Q.push(command[1])
+    elif command[0]=="pop":
+        Q.pop()
+    elif command[0]=="size":
+        Q.size()
+    elif command[0]=="top":
+        Q.top()
+    elif command[0]=="empty":
+        Q.empty()
