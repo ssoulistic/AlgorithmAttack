441d974d42f6bfc700c30fa6ef73aba4ce483984
ssoulistic <93485617+ssoulistic@users.noreply.github.com>
2023-11-06 23:27:26 +0900

    [Silver III] Title: 터렛, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1002. 터렛/터렛.py
@@ -0,0 +1,17 @@
+import sys
+T=int(sys.stdin.readline())
+for _ in range(T):
+    x1,y1,r1,x2,y2,r2=map(int,sys.stdin.readline().split())
+    if x1==x2 and y1==y2:
+        if r1==r2:
+            print(-1)
+        else:
+            print(0)
+    elif (x2-x1)**2+(y2-y1)**2==(r1+r2)**2 or (x2-x1)**2+(y2-y1)**2==(r1-r2)**2:
+        print(1)
+    elif (x2-x1)**2+(y2-y1)**2>(r1+r2)**2:
+        print(0)
+    elif (r1-r2)**2<(x2-x1)**2+(y2-y1)**2<(r1+r2)**2:
+        print(2)
+    else:
+        print(0)
