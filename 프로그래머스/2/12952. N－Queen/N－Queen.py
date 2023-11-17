8748923d1514cebb2658e21d3cb81f9d211f4d9d
ssoulistic <93485617+ssoulistic@users.noreply.github.com>
2023-11-17 18:36:19 +0900

    [level 2] Title: N-Queen, Time: 7322.46 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12952. N－Queen/N－Queen.py
@@ -0,0 +1,31 @@
+def solution(n):
+    global result
+    global T
+    global count
+    count=0
+    result=[]
+    T=n
+    queen(0)
+    return count
+
+def queen(col):
+    global count
+    if len(result)==T:
+        count+=1
+        return 
+    if col>=T:
+        return
+    # 위에서 부터 시작.
+    for row in range(T):
+        check=True
+        # 한개 선택 => 아래로 있는지 체크, 좌대각 우대각 체크 좌표로 넣기.
+        if result:
+            for x,y in result:
+                if y==row or abs(x-col)==abs(y-row):
+                    check=False
+                    break
+        if check:
+            result.append([col,row])
+            queen(col+1)
+            result.pop()
+
