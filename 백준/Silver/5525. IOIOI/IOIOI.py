4a3521b87a9fe121d67d62aad93254f3981c7666
ssoulistic <93485617+ssoulistic@users.noreply.github.com>
2023-12-19 11:29:05 +0900

    [Silver I] Title: IOIOI, Time: 340 ms, Memory: 34048 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,9 +1,18 @@
-N=int(input())
-M=int(input())
-S=input()
-PN="I"+"OI"*N
+N=int(input()) # N PN을 생성.I+OI*N회
+M=int(input()) # S의 길이
+S=input() # S
+P="I"+"OI"*N
+IOI=0
 answer=0
-for i in range(M-(2*N+1)+1):
-    if S[i:i+2*N+1]==PN:
-        answer+=1
+idx=0
+while idx<M-2:
+    if S[idx]=="I" and S[idx+1]=="O" and S[idx+2]=="I":
+        IOI+=1
+        if IOI==N:
+            IOI-=1
+            answer+=1
+        idx+=1
+    else:
+        IOI=0
+    idx+=1
 print(answer)
