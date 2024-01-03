48532e62858e09b818a31f553c64f4141d2105ef
ssoulistic <93485617+ssoulistic@users.noreply.github.com>
2024-01-03 18:18:17 +0900

    [Gold V] Title: 리모컨, Time: 2364 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.py
+++ 백준/Gold/1107. 리모컨/리모컨.py
@@ -1,3 +1,4 @@
+#브루트포스 풀이
 import sys
 target=int(sys.stdin.readline())
 start=100
@@ -7,31 +8,44 @@ rmc=[i for i in range(10)]
 for num in broken_numbers:
     rmc.remove(num)
 
-def click_able(n):
-    for s in str(n):
-        if int(s) not in rmc:
-            return False
-    return True
-
-# 가장 가까운수 만들기. 백트래킹 ㄱ
 answer=abs(target-start)
-def backt(guess,finish):
-    global answer
-    if finish<=0:
-        return
-    if len(guess)==finish:
-        gn=int("".join(guess))
-        if answer>abs(target-gn)+len(str(gn)):
-            answer=abs(target-gn)+len(str(gn))
-        return
-    for r in rmc:
-        guess.append(str(r))
-        backt(guess,finish)
-        guess.pop()
-    return
+for n in range(1000001):
+    Flag=True
+    for ni in str(n):
+        if int(ni) not in rmc:
+            Flag=False
+            break
+    if Flag:
+        answer=min(answer,abs(n-target)+len(str(n)))
+print(answer)    
 
-backt([],len(str(target))-1)
-backt([],len(str(target)))
-backt([],len(str(target))+1)
+# # 가장 가까운수 만들기. 백트래킹 풀이
+# target=int(input())
+# start=100
+# broke=int(input())
+# broken_numbers=list(map(int,input().split()))
+# rmc=[i for i in range(10)]
+# for num in broken_numbers:
+#     rmc.remove(num)
 
-print(answer)
+# answer=abs(target-start)
+# def backt(guess,finish):
+#     global answer
+#     if finish<=0:
+#         return
+#     if len(guess)==finish:
+#         gn=int("".join(guess))
+#         if answer>abs(target-gn)+len(str(gn)):
+#             answer=abs(target-gn)+len(str(gn))
+#         return
+#     for r in rmc:
+#         guess.append(str(r))
+#         backt(guess,finish)
+#         guess.pop()
+#     return
+
+# backt([],len(str(target))-1)
+# backt([],len(str(target)))
+# backt([],len(str(target))+1)
+
+# print(answer)
