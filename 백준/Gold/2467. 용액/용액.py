e2659b4701526a20720003b0343c27e884997fe5
ssoulistic <93485617+ssoulistic@users.noreply.github.com>
2024-01-23 20:27:44 +0900

    [Gold V] Title: 용액, Time: 156 ms, Memory: 42036 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.py
@@ -0,0 +1,17 @@
+import sys
+input=sys.stdin.readline
+N=int(input())
+liquid=sorted(map(int,input().split()))
+s=0
+e=len(liquid)-1
+mini=1e10
+pair=[]
+while s<e:
+    if abs(liquid[s]+liquid[e])<mini:
+        mini=abs(liquid[s]+liquid[e])
+        pair=[s,e]
+    if abs(liquid[s])<abs(liquid[e]):
+        e-=1
+    else:
+        s+=1    
+print(liquid[pair[0]],liquid[pair[1]])
