e2d3e9d8338f43170179a4e68c6b0c829f6371f0
ssoulistic <93485617+ssoulistic@users.noreply.github.com>
2024-03-28 18:27:42 +0900

    [Gold III] Title: 소문난 칠공주, Time: 2644 ms, Memory: 34096 KB -BaekjoonHub

---
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.py
@@ -0,0 +1,39 @@
+import sys
+input=sys.stdin.readline
+from itertools import combinations
+from collections import deque
+def bfs(start):
+    count=1
+    r,c=start
+    visited[r][c]=True
+    que=deque()
+    que.append([r,c])
+    search=[[-1,0],[1,0],[0,-1],[0,1]]
+    while que:
+        ri,ci=que.popleft()
+        for dr,dc in search:
+            nr=ri+dr
+            nc=ci+dc
+            if 0<=nr<5 and 0<=nc<5 and not visited[nr][nc]:
+                visited[nr][nc]=True
+                count+=1
+                que.append([nr,nc])
+    if count==7:
+        return True
+    else:
+        return False
+graph=[]
+for _ in range(5):
+    graph.append(list(input().strip()))   
+answer=[]
+for combo in combinations([[i,j] for i in range(5) for j in range(5)],7):
+    visited=[[True for _ in range(5)] for _ in range(5)]
+    som=0
+    for ri,ci in combo:
+        if graph[ri][ci]=="S":
+            som+=1
+        visited[ri][ci]=False
+    
+    if som>=4 and bfs(combo[0]):
+        answer.append(combo)
+print(len(answer))
