1bdbf7882aab1ea467ce84a7f8182a148d7ef64d
ssoulistic <93485617+ssoulistic@users.noreply.github.com>
2024-04-02 14:47:44 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 4308 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.py
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.py
@@ -7,27 +7,23 @@ for _ in range(N):
     S,W=map(int,input().split())
     eggs.append([S,W])
 
-def stroke(idx):
+def stroke(idx,cnt):
     global answer
-    state=list(map(lambda x: True if x[0]<=0 else False,eggs))
     if idx==N:
-        broken=state.count(True)
-        if broken>answer:   
-            answer=broken
+        if cnt>answer:   
+            answer=cnt
         return
-    if all(state):
-        answer=N
-        return
+    
     for i in range(N):
         if i!=idx:
             if eggs[i][0]>0 and eggs[idx][0]>0:
                 eggs[i][0]-=eggs[idx][1]
                 eggs[idx][0]-=eggs[i][1]
-                stroke(idx+1)
+                stroke(idx+1,cnt+int(eggs[i][0]<=0)+int(eggs[idx][0]<=0))
                 eggs[i][0]+=eggs[idx][1]
                 eggs[idx][0]+=eggs[i][1]
             else:
-                stroke(idx+1)
+                stroke(idx+1,cnt)
     return 
-stroke(0)
+stroke(0,0)
 print(answer)
