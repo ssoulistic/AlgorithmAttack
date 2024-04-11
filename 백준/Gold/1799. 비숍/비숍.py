e1b04a97f980f14ee73d94e8b9c59372b9fc2898
ssoulistic <93485617+ssoulistic@users.noreply.github.com>
2024-04-12 03:32:45 +0900

    [Gold I] Title: 비숍, Time: 3996 ms, Memory: 126564 KB -BaekjoonHub

---
+++ 백준/Gold/1799. 비숍/비숍.py
@@ -0,0 +1,32 @@
+import sys
+input=sys.stdin.readline
+N=int(input())
+chess=[]
+for p in range(N):
+    chess.append(list(map(int,input().split())))
+
+left_cross=[False for _ in range(2*N-1)]
+
+answer=0
+def bishop(right_coord,count):
+    global answer
+    if answer<count:
+        answer=count
+    if right_coord>2*N-1:
+        return
+    j=0
+    Flag=True
+    while j<=right_coord:
+        nr,nc=j,right_coord-j
+        if 0<=nr<N and 0<=nc<N:
+            if chess[nr][nc]==1 and not left_cross[N-1+nc-nr]:
+                Flag=False
+                left_cross[N-1+nc-nr]=True
+                bishop(right_coord+1,count+1)
+                left_cross[N-1+nc-nr]=False
+        j+=1
+    if Flag:
+        bishop(right_coord+1,count)
+        
+bishop(0,0)
+print(answer)
