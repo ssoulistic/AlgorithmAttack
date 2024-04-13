33cd32c9ac4108116c34ef2d453aa44ec798a693
ssoulistic <93485617+ssoulistic@users.noreply.github.com>
2024-04-13 14:46:12 +0900

    [Gold IV] Title: 알파벳, Time: 1604 ms, Memory: 168512 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,26 +1,31 @@
 import sys
 input=sys.stdin.readline
+from collections import deque
 R,C=map(int,input().split())
 graph=[]
 answer=0
+visited=[[set() for _ in range(C)] for _ in range(R)]
 for _ in range(R):
     graph.append(list(input().strip()))
-alpha=[False for _ in range(26)]
-
-def dfs(coord,count):
-    global answer 
-    r,c=coord
-    idx_now=ord(graph[r][c])-ord("A")
-    answer=max(answer,count)
+def bfs(start):
+    global answer
+    r,c=start
+    history=deque()
+    history.append([[r,c],graph[r][c]])
+    visited[r][c].add(graph[r][c])
+    search=[[-1,0],[1,0],[0,-1],[0,1]]
+    while history:
+        [ri,ci],string=history.popleft()
+        if len(string)>answer:
+            answer=len(string)
+        for dr,dc in search:
+            nr=ri+dr
+            nc=ci+dc
+            if 0<=nr<R and 0<=nc<C and graph[nr][nc] not in string:
+                next_string=string+graph[nr][nc]
+                if next_string not in visited[nr][nc]:
+                    visited[nr][nc].add(next_string)
+                    history.append([[nr,nc],next_string])
     
-    for dr,dc in [[-1,0],[1,0],[0,-1],[0,1]]:
-        nr=r+dr
-        nc=c+dc
-        if 0<=nr<R and 0<=nc<C and not alpha[ord(graph[nr][nc])-ord("A")]:
-            alpha[ord(graph[nr][nc])-ord("A")]=True
-            dfs([nr,nc],count+1)
-            alpha[ord(graph[nr][nc])-ord("A")]=False
-
-alpha[ord(graph[0][0])-ord("A")]=True
-dfs([0,0],1)
+bfs([0,0])
 print(answer)
