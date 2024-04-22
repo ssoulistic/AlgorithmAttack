bc506732e5d6333a7cc67c7a5b9f9303ee828899
ssoulistic <93485617+ssoulistic@users.noreply.github.com>
2024-04-22 15:23:07 +0900

    [Gold V] Title: 빗물, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.py
+++ 백준/Gold/14719. 빗물/빗물.py
@@ -4,9 +4,6 @@ input=sys.stdin.readline
 H,W=map(int,input().split())
 blocks=list(map(int,input().split()))
 answer=0
-# 존재하는 기둥중 가장 왼쪽
-# stack[-1] 그보다 작은수가 나오면 stack에 쌓기.
-
 # 시작 => 자기보다 크거나 같은게 나올때까지 탐색
 # 나오면 => 그 곳 부터 또 다시 시작
 # 안나오면 => 그것보다 1 작은 수로 다시 시도.
