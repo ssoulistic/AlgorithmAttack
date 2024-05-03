0d702b7cf600125978d2526d83d9a0cb0072672a
ssoulistic <93485617+ssoulistic@users.noreply.github.com>
2024-05-03 22:52:51 +0900

    [Gold III] Title: 내리막 길, Time: 140 ms, Memory: 43604 KB -BaekjoonHub

---
+++ 백준/Gold/1520. 내리막 길/내리막 길.py
@@ -0,0 +1,25 @@
+import sys
+input=sys.stdin.readline
+
+M,N=map(int,input().split())
+graph=[]
+dp=[[0 for _ in range(N)] for _ in range(M)]
+dp[M-1][N-1]=1
+for _ in range(M):
+    graph.append(list(map(int,input().split())))
+
+search=[[1,0],[0,1],[-1,0],[0,-1]]
+def dfs(r,c):
+    if dp[r][c]:
+        return dp[r][c]
+    
+    for dr,dc in search:
+        nr=r+dr
+        nc=c+dc
+        if 0<=nr<M and 0<=nc<N and graph[nr][nc]<graph[r][c] and graph[nr][nc]>=graph[M-1][N-1]:
+            dp[r][c]+=dfs(nr,nc)
+    if dp[r][c]==0:
+        graph[r][c]=1e9
+    return dp[r][c]
+dfs(0,0)
+print(dp[0][0])
