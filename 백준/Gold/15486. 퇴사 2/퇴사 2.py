ed8124d28687b9cbb066a29148c71fa289c5ee6e
ssoulistic <93485617+ssoulistic@users.noreply.github.com>
2024-05-04 18:46:31 +0900

    [Gold V] Title: 퇴사 2, Time: 2388 ms, Memory: 97136 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -0,0 +1,17 @@
+import sys
+input=sys.stdin.readline 
+N=int(input())
+dp=[0 for _ in range(N+1)]
+seq=[]
+befmax=0
+for idx in range(N):
+    T,P=map(int,input().split())
+    start=idx
+    end=idx+T
+    earn=P
+
+    befmax=max(dp[start],befmax)
+    if 0<=end<=N:
+        dp[end]=max(dp[end],befmax+earn)
+    
+print(max(dp))
