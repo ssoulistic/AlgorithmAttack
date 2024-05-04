89ebbe641d8c36f698bee224fc66c01b3269db58
ssoulistic <93485617+ssoulistic@users.noreply.github.com>
2024-05-04 18:49:02 +0900

    [Silver III] Title: 퇴사, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,25 +1,17 @@
 import sys
 input=sys.stdin.readline 
 N=int(input())
-dp=[0 for _ in range(N)]
+dp=[0 for _ in range(N+1)]
 seq=[]
+befmax=0
+answer=0
 for idx in range(N):
     T,P=map(int,input().split())
-    seq.append([idx,T,P])
-
-def dfs(day,acc):
-    dp[day]=max(dp[day],acc)
-    for next in range(day+1,N):
-        start,take,earn=seq[next]
-        if start+take-1<N:
-            dfs(start+take-1,acc+earn)
-        else:
-            dfs(next,acc)
-    return
-
-dfs(0,0)
-s,t,e=seq[0]
-if s+t-1<N:
-    dfs(s+t-1,e)
-
-print(max(dp))
+    start=idx
+    end=idx+T
+    earn=P
+    befmax=max(dp[start],befmax)
+    if 0<=end<=N:
+        dp[end]=max(dp[end],befmax+earn)
+        answer=max(dp[end],answer)
+print(answer)
