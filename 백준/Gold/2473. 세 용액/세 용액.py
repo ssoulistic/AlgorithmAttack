472fa14c262460c3bd71a1733a8e9126070c9d18
ssoulistic <93485617+ssoulistic@users.noreply.github.com>
2024-05-18 18:11:32 +0900

    [Gold III] Title: 세 용액, Time: 4780 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.py
+++ 백준/Gold/2473. 세 용액/세 용액.py
@@ -1,26 +1,32 @@
 import sys
-input=sys.stdin.readline
-N=int(input())
-liq=list(map(int,input().split()))
-liq.sort()
-minimum=1e10
-memo=[]
+input = sys.stdin.readline
 
-for a in range(N):
-    for b in range(a+1,N):
-        s=b+1
-        e=N-1
-        while s<e<N:
-            mid=(s+e)//2
-            if liq[a]+liq[b]+liq[mid]<0:
-                s=mid+1
-            else:
-                e=mid
-        if a<b<e and  minimum>abs(liq[a]+liq[b]+liq[e]):
-            minimum=abs(liq[a]+liq[b]+liq[e])
-            memo=[a,b,e]
-        if a<b<e-1 and  minimum>abs(liq[a]+liq[b]+liq[e-1]):
-            minimum=abs(liq[a]+liq[b]+liq[e-1])
-            memo=[a,b,e-1]
+# 입력 받기
+N = int(input())
+liq = list(map(int, input().split()))
+liq.sort()  # 정렬
 
-print(*map(lambda x: liq[x],memo))
+minimum = float('inf')  # 최소 절대값을 큰 값으로 초기화
+result = []  # 결과 저장용 리스트
+
+# 고정된 용액 a를 선택하고 나머지 두 용액을 투 포인터로 찾음
+for i in range(N-2):
+    left = i + 1
+    right = N - 1
+    
+    while left < right:
+        mix = liq[i] + liq[left] + liq[right]
+        
+        # 최소 절대값 갱신
+        if abs(mix) < minimum:
+            minimum = abs(mix)
+            result = [liq[i], liq[left], liq[right]]
+        
+        # 특성값이 0에 가까워지도록 포인터 이동
+        if mix < 0:
+            left += 1
+        else:
+            right -= 1
+
+# 결과 출력
+print(*result)
