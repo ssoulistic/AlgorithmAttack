b0f5dc8df54e7b7b163458bda7ac5fde4143a324
ssoulistic <93485617+ssoulistic@users.noreply.github.com>
2024-05-21 22:23:04 +0900

    [Gold IV] Title: 비슷한 단어, Time: 132 ms, Memory: 43404 KB -BaekjoonHub

--- 백준/Gold/2179. 비슷한 단어/비슷한 단어.py
+++ 백준/Gold/2179. 비슷한 단어/비슷한 단어.py
@@ -1,24 +1,21 @@
 import sys
 input=sys.stdin.readline
 N=int(input())
+prefix={}
 words=[]
-for _ in range(N):
-    words.append(input().strip())
-
-answer=[0,[]]
-for i in range(N):
-    if len(words[i])<=answer[0]:
-        continue
-    for j in range(i+1,N):
-        if len(words[j])<=answer[0] or words[i]==words[j]:
-            continue
-        count=0
-        for k in range(min(len(words[i]),len(words[j]))):
-            if words[i][k]==words[j][k]:
-                count+=1
-            else:
-                break
-        if answer[0]<count:
-            answer[0]=count
-            answer[1]=[words[i],words[j]]
-print(*answer[1],sep="\n")
+for idx in range(N):
+    word=input().strip()
+    words.append(word)
+    for i in range(1,len(word)+1):
+        char=word[:i]
+        if prefix.get(char):
+            prefix[char].append(idx)
+        else:
+            prefix[char]=[idx]
+longest=[0,[1e9,1e9]]
+for k,v in prefix.items():
+    if len(k)>longest[0]:
+        if len(v)>1:
+            longest[0]=len(k)
+            longest[1]=[v[0],v[1]]
+print(words[longest[1][0]],words[longest[1][1]],sep="\n")
