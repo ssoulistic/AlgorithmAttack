f5d61847d87cfed8259c97c0f23e288210d0c8a6
ssoulistic <93485617+ssoulistic@users.noreply.github.com>
2024-05-22 15:02:40 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,14 +1,15 @@
+from collections import deque
 def solution(bridge_length, weight, truck_weights):
-    bridge=[]
-    stamp=[]
-    time=0
-    # 모두다 건너면
-    while sum(bridge) or truck_weights:
-        if truck_weights and sum(bridge)+truck_weights[0]<=weight:
-            bridge.append(truck_weights.pop(0))
-            stamp.append(time+bridge_length-1)
-        if bridge and stamp[0]==time:
-            bridge.pop(0)
-            stamp.pop(0)
-        time+=1
-    return time+1
+    arrived=[]
+    N=len(truck_weights)
+    bridge=deque()
+    count=0
+    i=0
+    while len(arrived)<N:
+        if bridge and bridge[0][1]+bridge_length==count:
+            arrived.append(bridge.popleft())            
+        if i<N and sum(map(lambda x: x[0],bridge))+truck_weights[i]<=weight:
+            bridge.append([truck_weights[i],count])
+            i+=1
+        count+=1
+    return count
