a8b26a9b1c207011a7cfdc5d69c08223abf54cc2
ssoulistic <93485617+ssoulistic@users.noreply.github.com>
2024-05-23 22:22:50 +0900

    [level 2] Title: 주식가격, Time: 14.82 ms, Memory: 16.9 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,8 +1,13 @@
 def solution(prices):
-    answer = []
-    for i in range(len(prices)):
-        for j in range(i+1,len(prices)):
-            if prices[i]>prices[j]:
-                break
-        answer.append(j-i)                
+    n=len(prices)
+    answer = [0]*n
+    stack = []
+    for i in range(n):
+        while stack and prices[stack[-1]]>prices[i]:
+            j=stack.pop()
+            answer[j]=i-j
+        stack.append(i)
+    while stack:
+        j=stack.pop()
+        answer[j]=n-1-j
     return answer
