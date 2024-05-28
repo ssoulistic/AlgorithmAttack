f7f53dcf8302ebbf9778e9e5d8572c28a5263ca4
ssoulistic <93485617+ssoulistic@users.noreply.github.com>
2024-05-28 15:33:46 +0900

    [level 2] Title: 모음 사전, Time: 9.67 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -0,0 +1,14 @@
+def solution(word):
+    words=[]
+    def backt(word_stack):
+        if len(word_stack)>5:
+            return
+        words.append("".join(word_stack))
+        for alpha in ["A","E","I","O","U"]:
+            backt(word_stack+[alpha])
+    backt([])
+    dic={}
+    words.sort()
+    for i in range(len(words)):
+        dic[words[i]]=i
+    return dic[word]
