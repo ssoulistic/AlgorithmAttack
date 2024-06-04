50bdc96145714bf4e976302d58efd347d6d9c093
이서호 <93485617+ssoulistic@users.noreply.github.com>
2024-06-04 16:43:39 +0900

    [level 3] Title: 섬 연결하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,24 +1,27 @@
 def solution(n, costs):
+    # 간선 위주 => 크루스칼 알고리즘
+    # 최소 비용의 간선순으로 사이클이 발생 하지 않도록 연결
+    # 사이클 발생여부는 유니온 파인드로 확인.
+    
+    # 유니온 파인드 정의 구간
+    group=[i for i in range(n)]
     def find(x):
         if group[x]!=x:
             group[x]=find(group[x])
         return group[x]
+    
     def union(a,b):
-        a=find(a)
-        b=find(b)
-        if a>b:
-            group[a]=b
-            return True
-        elif a<b:
-            group[b]=a
+        a,b=find(a),find(b)
+        if a!=b:
+            group[min(a,b)]=max(a,b)
             return True
         return False
     
-    costs.sort(key=lambda x: -x[2])
-    group=[i for i in range(n)]
+    #deque 불러오기싫어서 역순으로 정렬
     answer = 0
+    costs.sort(key=lambda x: -x[2])
     while costs:
-        x,y,val=costs.pop()
-        if union(x,y):
-            answer+=val
+        start,end,value=costs.pop()
+        if union(start,end):
+            answer+=value
     return answer
