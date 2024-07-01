e971c498ad9ea62eb38c606279ebf7042f438e9a
Seo Ho Lee <93485617+ssoulistic@users.noreply.github.com>
2024-07-01 23:04:47 +0900

    [Gold III] Title: 파일 합치기, Time: 2152 ms, Memory: 123132 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.py
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.py
@@ -7,12 +7,11 @@ for _ in range(T):
     acc_fs=[0]
     for p in range(K):
         acc_fs.append(acc_fs[-1]+file_sizes[p])
-    dp=[[float("inf")]*K for _ in range(K)]
-    for j in range(K):
-        dp[j][j]=0
+    dp=[[0]*K for _ in range(K)]
     for length in range(1,K+1):
         for row in range(K-length):
             col=row+length
+            dp[row][col]=float("inf")
             for i in range(length):
                 if dp[row][col]>dp[row+length-i][col]+dp[row][col-1-i]+acc_fs[col+1]-acc_fs[row]:
                     dp[row][col]=dp[row+length-i][col]+dp[row][col-1-i]+acc_fs[col+1]-acc_fs[row]
