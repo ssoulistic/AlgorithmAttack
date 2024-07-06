886740eb6cede760d2fca654fdf67f15e469387b
Seo Ho Lee <93485617+ssoulistic@users.noreply.github.com>
2024-07-07 00:15:33 +0900

    [Gold III] Title: 텀 프로젝트, Time: 3588 ms, Memory: 49284 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
@@ -2,18 +2,16 @@ import sys
 input=sys.stdin.readline 
 
 T=int(input())
-
 def dfs(cur):
+    current_list=list()
     while True:
         if visited[cur]:
-            for l in range(len(current_list)):
-                if current_list[l]==cur:
-                    return len(current_list)-l
+            if cur in current_list:
+                return len(current_list)-current_list.index(cur)
             return 0
         current_list.append(cur)
         visited[cur]=True
         cur=sel[cur]
-
 
 for _ in range(T):
     n=int(input())
@@ -23,8 +21,6 @@ for _ in range(T):
     for j in range(n):
         sel[j+1]=students[j]
     visited=[False for _ in range(n+1)]
-    teamed=[False for _ in range(n+1)]
     for k in range(1,n+1):
-        current_list=list()
         answer-=dfs(k)
     print(answer)
