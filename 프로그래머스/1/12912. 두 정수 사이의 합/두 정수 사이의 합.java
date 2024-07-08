e8c1b5dca7b8d741d02a1237322f1b64ba247d3f
Seo Ho Lee <93485617+ssoulistic@users.noreply.github.com>
2024-07-08 09:03:36 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.13 ms, Memory: 70.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.java
@@ -0,0 +1,18 @@
+class Solution {
+    public long solution(int a, int b) {
+        long answer = 0;
+        int min,max;
+        if (a>b){
+            min=b;
+            max=a;
+        }
+        else{
+            min=a;
+            max=b;
+        }
+        for (int i=min;i<=max;i++){
+            answer+=i;
+        }
+        return answer;
+    }
+}
