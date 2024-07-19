8c2509aa9e103cb6e4d20adaddb142df5b433e4a
Seo Ho Lee <93485617+ssoulistic@users.noreply.github.com>
2024-07-20 07:05:23 +0900

    [level 1] Title: 음양 더하기, Time: 0.07 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -3,12 +3,7 @@ class Solution {
         int answer = 0;
         for (int i =0; i<absolutes.length;i++)
         {
-            if (signs[i]==true){
-                answer+=absolutes[i];
-            }
-            else{
-                answer-=absolutes[i];
-            }
+            answer+=absolutes[i]*(signs[i]?1:-1);
             
         }
         return answer;
