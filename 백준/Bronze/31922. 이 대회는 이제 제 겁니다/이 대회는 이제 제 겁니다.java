3653563813a3b334dfaec513e580f65462a147fb
Seo Ho Lee <93485617+ssoulistic@users.noreply.github.com>
2024-10-09 17:20:59 +0900

    [Bronze IV] Title: 이 대회는 이제 제 겁니다, Time: 100 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Bronze/31922. 이 대회는 이제 제 겁니다/이 대회는 이제 제 겁니다.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int A = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        bw.write(String.valueOf(Math.max(A + C, P)));
+        bw.flush();
+    }
+}
