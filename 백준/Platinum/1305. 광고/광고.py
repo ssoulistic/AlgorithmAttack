cc11a26777651e7cd440edfff3770405a5c2039e
ssoulistic <93485617+ssoulistic@users.noreply.github.com>
2024-10-27 11:53:14 +0900

    [Platinum IV] Title: 광고, Time: 528 ms, Memory: 73144 KB -BaekjoonHub

---
+++ 백준/Platinum/1305. 광고/광고.py
@@ -0,0 +1,22 @@
+import sys
+input=sys.stdin.readline
+L=int(input())
+T=input().strip()
+def F(word):
+    l=len(word)
+    table=[0 for _ in range(l)]
+    pattern=1
+    
+    prefix_idx=0
+    for idx in range(1,l):
+        while prefix_idx>0 and word[prefix_idx]!=word[idx]:
+            prefix_idx=table[prefix_idx-1]
+        if word[prefix_idx]==word[idx]:
+            prefix_idx+=1
+            table[idx]=prefix_idx
+        pattern=max(pattern,idx-prefix_idx+1)
+        
+    return pattern
+
+
+print(F(T))
