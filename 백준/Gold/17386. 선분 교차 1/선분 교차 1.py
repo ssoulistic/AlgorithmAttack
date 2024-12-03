6ac9695e7ea84c4d9ef93d4616d88f5b88c14ed8
ssoulistic <93485617+ssoulistic@users.noreply.github.com>
2024-12-03 14:30:28 +0900

    [Gold III] Title: 선분 교차 1, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/17386. 선분 교차 1/선분 교차 1.py
@@ -0,0 +1,22 @@
+import sys
+input=sys.stdin.readline
+x1,y1,x2,y2=map(int,input().split())
+x3,y3,x4,y4=map(int,input().split())
+
+def ccw(p1,p2,p3):
+    return (p2[0]-p1[0])*(p3[1]-p1[1])-(p2[1]-p1[1])*(p3[0]-p1[0])
+
+A=[x1,y1]
+B=[x2,y2]
+C=[x3,y3]
+D=[x4,y4]
+if A>B:
+    A,B=B,A
+if C>D:
+    C,D=D,C
+
+
+if ccw(A,B,C)*ccw(A,B,D)<=0 and ccw(C,D,A)*ccw(C,D,B)<=0 and A<=D and C<=B:
+    print(1)
+else:
+    print(0)
