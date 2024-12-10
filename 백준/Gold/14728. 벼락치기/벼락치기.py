0741235da23f4e354cef3fc8d81cf315f8df14df
ssoulistic <93485617+ssoulistic@users.noreply.github.com>
2024-12-11 01:22:12 +0900

    [Gold V] Title: 벼락치기, Time: 516 ms, Memory: 55380 KB -BaekjoonHub

--- 백준/Gold/14728. 벼락치기/벼락치기.py
+++ 백준/Gold/14728. 벼락치기/벼락치기.py
@@ -7,9 +7,10 @@ for _ in range(N):
     K,S=map(int,input().split())
     work.append([K,S])
 for idx in range(1,N+1):
+    K,S=work[idx-1]
     for time in range(1,T+1):
-        if time-work[idx-1][0]>=0:
-            dp[time][idx]=max(dp[time-work[idx-1][0]][idx-1]+work[idx-1][1],dp[time][idx-1])
+        if time-K>=0:
+            dp[time][idx]=max(dp[time-K][idx-1]+S,dp[time][idx-1])
         else:
             dp[time][idx]=dp[time][idx-1]
 print(dp[-1][-1])
