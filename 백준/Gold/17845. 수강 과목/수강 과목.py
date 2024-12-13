1bacb2c784d4b9a3462e1a2684d57f78abbe3942
ssoulistic <93485617+ssoulistic@users.noreply.github.com>
2024-12-13 11:50:57 +0900

    [Gold V] Title: 수강 과목, Time: 1664 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/17845. 수강 과목/수강 과목.py
+++ 백준/Gold/17845. 수강 과목/수강 과목.py
@@ -5,5 +5,7 @@ dp=[0 for _ in range(N+1)]
 for _ in range(1,K+1):
     I,T = map(int,input().split()) #Importance,Time
     for t in range(N,T-1,-1):
-        dp[t]=max(dp[t-T]+I,dp[t])
+        now=dp[t-T]+I
+        if dp[t]<now:
+            dp[t]=now
 print(dp[-1])
