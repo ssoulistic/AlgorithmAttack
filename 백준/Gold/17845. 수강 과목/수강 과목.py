08aec06099af069396de633a6e7d9ed0cefedc83
ssoulistic <93485617+ssoulistic@users.noreply.github.com>
2024-12-13 11:43:30 +0900

    [Gold V] Title: 수강 과목, Time: 2028 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/17845. 수강 과목/수강 과목.py
+++ 백준/Gold/17845. 수강 과목/수강 과목.py
@@ -1,12 +1,9 @@
 import sys
 input=sys.stdin.readline
 N,K=map(int,input().split())
-dp=[[0 for _ in range(N+1)] for _ in range(K+1)]
-for i in range(1,K+1):
+dp=[0 for _ in range(N+1)]
+for _ in range(1,K+1):
     I,T = map(int,input().split()) #Importance,Time
-    for t in range(1,N+1):
-        if t>=T:
-            dp[i][t]=max(dp[i-1][t-T]+I,dp[i-1][t])
-        else:
-            dp[i][t]=dp[i-1][t]
-print(dp[-1][-1])
+    for t in range(N,T-1,-1):
+        dp[t]=max(dp[t-T]+I,dp[t])
+print(dp[-1])
