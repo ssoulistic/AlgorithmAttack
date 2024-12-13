8fe143142329e347424a64bfa275db2bd1228d57
ssoulistic <93485617+ssoulistic@users.noreply.github.com>
2024-12-13 11:54:52 +0900

    [Gold V] Title: 수강 과목, Time: 688 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/17845. 수강 과목/수강 과목.py
+++ 백준/Gold/17845. 수강 과목/수강 과목.py
@@ -1,11 +1,13 @@
 import sys
 input=sys.stdin.readline
-N,K=map(int,input().split())
-dp=[0 for _ in range(N+1)]
-for _ in range(1,K+1):
-    I,T = map(int,input().split()) #Importance,Time
-    for t in range(N,T-1,-1):
-        now=dp[t-T]+I
-        if dp[t]<now:
-            dp[t]=now
-print(dp[-1])
+def solve():
+    N,K=map(int,input().split())
+    dp=[0 for _ in range(N+1)]
+    for _ in range(K):
+        I,T = map(int,input().split()) #Importance,Time
+        for t in range(N,T-1,-1):
+            now=dp[t-T]+I
+            if dp[t]<now:
+                dp[t]=now
+    print(dp[N])
+solve()
